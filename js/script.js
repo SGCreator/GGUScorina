876ef51c07ba1661af3b0b84042ba7d764b243c5
SGCreator <77673860+SGCreator@users.noreply.github.com>
2021-04-20 22:03:27 +0300

    added 20 04 2021

--- js/script.js
+++ js/script.js
@@ -9,17 +9,20 @@ burger.addEventListener('click', function(){
 let slider = (sliderSel,images) => {
     const slide = document.querySelector(sliderSel)
     let i = 1
-
-    function toggleSlide (n,img) {
-        setInterval(function(){
-            slide.style.background = `url('${img[n]}') no-repeat 0 60px / cover`
-            n++
-            if(n >= img.length){
-                n = 0
-            }
-        },8000)
+    if(slide){
+        function toggleSlide (n,img) {
+            setInterval(function(){
+                slide.style.background = `url('${img[n]}') no-repeat 0 60px / cover`
+                n++
+                if(n >= img.length){
+                    n = 0
+                }
+            },8000)
+        }
+        toggleSlide(i,images)
+    } else {
+        return false
     }
-    toggleSlide(i,images)
 }
 slider('.full-block-slider', ['images/main-slider/slider1.jpg','images/main-slider/slider2.jpg']);
 function map(n) {
